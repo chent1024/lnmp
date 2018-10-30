c6a2bb79d841620861285a92027b2fa9ec62264d
chent <emailctao@163.com>
2018-10-30 10:06:42 +0800

    up

--- include/common.sh
+++ include/common.sh
@@ -15,17 +15,16 @@ Install_Deps()
 {
     echo "[-] Yum install deps packages ..."
     yum -y update
-    yum -y install wget lrzsz ca-certificates \
-        make cmake gcc autoconf gcc-c++ gcc-g77 file pcre-devel \
+    yum -y install wget ca-certificates \
+        make cmake gcc autoconf gcc-c++ file pcre-devel \
         libtool libtool-libs \
         libjpeg libjpeg-devel libpng libpng-devel libpng10 libpng10-devel gd gd-devel freetype freetype-devel \
         libxml2 libxml2-devel \
         zlib zlib-devel glib2 glib2-devel bzip2 bzip2-devel \
         libevent libevent-devel \
         ncurses ncurses-devel curl curl-devel libcurl-devel \
-        e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel \
         openssl openssl-devel \
-        gettext gettext-devel ncurses-devel gmp-devel pspell-devel unzip
+        gettext gettext-devel gmp-devel pspell-devel unzip
 
     sh -c 'echo /usr/local/lib > /etc/ld.so.conf.d/local.conf'
     ldconfig -v
